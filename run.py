3f86a2f5e5bd751c488e558d5b2ee95e390068ed
Florian Guldner <florian.guldner@googlemail.com>
2024-07-08 23:14:15 +0000

    add \n in input elements, relevatn for deployment

--- run.py
+++ run.py
@@ -29,7 +29,7 @@ def show_portfolio():
 
 
 def delete_stock_column():
-    stock_name = input("Enter the name of the stock to delete: ")
+    stock_name = input("Enter the name of the stock to delete:\n ")
     cell = stock_portfolio.find(stock_name)
     if cell:
         stock_portfolio.delete_columns(cell.col)
@@ -39,11 +39,11 @@ def delete_stock_column():
 
 
 def add_stock_column():
-    new_stock_name = input("Enter the name of the new stock: ")
+    new_stock_name = input("Enter the name of the new stock:\n ")
     last_column = len(stock_portfolio.row_values(1))
     stock_portfolio.update_cell(1, last_column + 1, new_stock_name)
     print(f"New column for stock '{new_stock_name}' added.")
-    new_multiplicator = input(f"Enter the new multiplicator for {stock_name} (integer): ")
+    new_multiplicator = input(f"Enter the new multiplicator for {stock_name} (integer): \n")
     try:
         new_multiplicator = int(new_multiplicator)
         stock_portfolio.update_cell(2, cell.col, new_multiplicator)
@@ -52,10 +52,10 @@ def add_stock_column():
         print("Invalid input. Please enter a full number.")
 
 def adjust_multiplicator():
-    stock_name = input("Enter the name of the stock to adjust the multiplicator: ")
+    stock_name = input("Enter the name of the stock to adjust the multiplicator:\n ")
     cell = stock_portfolio.find(stock_name)
     if cell:
-        new_multiplicator = input(f"Enter the new multiplicator for {stock_name} (integer): ")
+        new_multiplicator = input(f"Enter the new multiplicator for {stock_name} (integer):\n ")
         try:
             new_multiplicator = int(new_multiplicator)
             stock_portfolio.update_cell(2, cell.col, new_multiplicator)
@@ -120,44 +120,47 @@ def show_low_performers():
 
     return stocks_decreasing    
 
-def calculate_profit_loss(): # not working yet!
+# def calculate_profit_loss(): # not working yet!
 # take the last value of a column and substract the first (-1) value of the column from sheet stock_daily_update
 # multiply the value with the multiplicator in the second row from sheet stock_portfolio
 # add the value to the second row of sheet profit_and_loss
-    header = stock_daily_update.row_values(1)
+#    header = stock_daily_update.row_values(1)
 
-    for col_index in range(1, len(header) + 1):
-        column_values = stock_daily_update.col_values(col_index)[1:]  # Exclude header
-        first_column_value = column_values[0]
-        last_column_value = column_values[-1]
+#   for col_index in range(1, len(header) + 1):
+#        column_values = stock_daily_update.col_values(col_index)[1:]  # Exclude header
+#        first_column_value = column_values[0]
+#        last_column_value = column_values[-1]
 
         # profit_loss_value = last_column_value - first_column_value !! float, integr needed. Currently string format
 
-        print(f"Processing column: ")
-        print(f" First value in column {header[col_index - 1]} is:" + first_column_value)
-        print(f" Last value in column {header[col_index - 1]} is:" + last_column_value)
-        print(f" profit and loss for {header[col_index - 1]} is:" + profit_loss_value)
+#        print(f"Processing column: ")
+#        print(f" First value in column {header[col_index - 1]} is:" + first_column_value)
+#        print(f" Last value in column {header[col_index - 1]} is:" + last_column_value)
+#        print(f" profit and loss for {header[col_index - 1]} is:" + profit_loss_value)
 
         #check zip function loveSandwich
-        profit_loss_sheet.find{header[col_index - 1]}
-        column_values_multiplicator = profit_loss_sheet.col_values(col_index)[1:]  # Exclude header
-        profit_loss_total = profit_loss_value * column_values_multiplicator[-1] # Werte zweite Zeile * profit_loss
-        profit_loss_total_minus_brokerfee
-        profit_loss_total_minus_brokerfee_minus_taxes #ignore free german allowance right now, maybe add later - only pay tax when surplus is positive
+#        profit_loss_sheet.find {header[col_index - 1]}
+#        column_values_multiplicator = profit_loss_sheet.col_values(col_index)[1:]  # Exclude header
+#        profit_loss_total = profit_loss_value * column_values_multiplicator[-1] # Werte zweite Zeile * profit_loss
+#        profit_loss_total_minus_brokerfee
+#        profit_loss_total_minus_brokerfee_minus_taxes #ignore free german allowance right now, maybe add later - only pay tax when surplus is positive
 
         #add profit_loss_total value to the sheet "profit_loss"
-        profit_loss_sheet.append_row(data)
+#        profit_loss_sheet.append_row(data)
 
-        if profit_loss_total_minus_brokerfee_minus_taxes > 0:
-            print(f"Profit!")
-        else:
-            print(f"Loss!")
+#        if profit_loss_total_minus_brokerfee_minus_taxes > 0:
+#            print(f"Profit!")
+#        else:
+#            print(f"Loss!")
 
-            
+
 print('Welcome to Stock Analyst. Get an overview of your portfolio')
+
+
 
 def main():
     show_portfolio()
+    # add ascci art! https://www.youtube.com/watch?v=Y0QiBbI3MWs, https://www.geeksforgeeks.org/generate-simple-ascii-tables-using-prettytable-in-python/, https://github.com/ericm/stonks
 
     while True:
         print("\nOptions:")
@@ -168,6 +171,7 @@ def main():
         print("5: Show low performer")
         print("6: Show profit and loss")
         print("7: Exit")
+        #print("8: Show me a graphic from stock: ")
 
         choice = input("Choose an option (1, 2, 3, 4, 5, 6, 7): ")
         
