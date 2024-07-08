ca8ebc04b2197ac84e299a62338ace12f60249d9
Florian Guldner <florian.guldner@googlemail.com>
2024-07-08 19:45:03 +0000

    add code for adjusting sheet stock_portfolio
    a
    clear

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-import gspread # imports the whole libary
-from google.oauth2.service_account import Credentials #make sure just to import relevant parts of the libary
+
+# imports the whole libary
+import gspread
+
+#make sure just to import relevant parts of the libary
+from google.oauth2.service_account import Credentials 
 from pprint import pprint
 
+# IAM - list of google-APIs that the program should access in order to run
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -10,133 +15,86 @@ SCOPE = [
 ]
 
 CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS = CREDS.with_scopes(SCOPE)
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+SHEET = GSPREAD_CLIENT.open('stock_analyst')
 
-#sales = SHEET.worksheet('sales')
-#data = sales.get_all_values()
-#print(data)
+stock_portfolio = SHEET.worksheet('stock_portfolio')
 
-def get_sales_data():
-    """
-    Get sales figures input from the user.
-    Run a while loop to collect a valid string of data from the user
-    via the terminal, which must be a string of 6 numbers separated
-    by commas. The loop will repeatedly request data, until it is valid.
-    """
-    while True:
-        print("Please enter sales data from the last market.")
-        print("Data should be six numbers, separated by commas.")
-        print("Example: 10,20,30,40,50,60\n")
+def show_portfolio():
+    data = stock_portfolio.get_all_values()
+    print(data)
 
-        data_str = input("Enter your data here: ")
-    
-        sales_data = data_str.split(",")
 
-        if validate_data(sales_data):
-            print('Data is valid!')
-            break
+def delete_stock_column():
+    stock_name = input("Enter the name of the stock to delete: ")
+    cell = stock_portfolio.find(stock_name)
+    if cell:
+        stock_portfolio.delete_columns(cell.col)
+        print(f"Column for stock '{stock_name}' deleted.")
+    else:
+        print(f"Stock '{stock_name}' not found. Please check your spelling. Otherwise the stock is not available in the list, have a look on the overview above.")
 
-    return sales_data
 
-def validate_data(values):
-    """
-    Inside the try, converts all string values into integers.
-    Raises ValueError if strings cannot be converted into int,
-    or if there aren't exactly 6 values.
-    """
-
-    print(values)
-
+def add_stock_column():
+    new_stock_name = input("Enter the name of the new stock: ")
+    last_column = len(stock_portfolio.row_values(1))
+    stock_portfolio.update_cell(1, last_column + 1, new_stock_name)
+    print(f"New column for stock '{new_stock_name}' added.")
+    new_multiplicator = input(f"Enter the new multiplicator for {stock_name} (integer): ")
     try:
-        [int(value) for value in values] #converts each value to an integer
-        if len(values) != 6:
-            raise ValueError(
-                f"Exactly 6 values required, you provided {len(values)}"
-            )
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again \n")
-        return False
+        new_multiplicator = int(new_multiplicator)
+        stock_portfolio.update_cell(2, cell.col, new_multiplicator)
+        print(f"Multiplicator for stock '{stock_name}' updated to {new_multiplicator}.")
+    except ValueError:
+        print("Invalid input. Please enter a full number.")
 
-    return True
+def adjust_multiplicator():
+    stock_name = input("Enter the name of the stock to adjust the multiplicator: ")
+    cell = stock_portfolio.find(stock_name)
+    if cell:
+        new_multiplicator = input(f"Enter the new multiplicator for {stock_name} (integer): ")
+        try:
+            new_multiplicator = int(new_multiplicator)
+            stock_portfolio.update_cell(2, cell.col, new_multiplicator)
+            print(f"Multiplicator for stock '{stock_name}' updated to {new_multiplicator}.")
+        except ValueError:
+            print("Invalid input. Please enter a full number.")
+    else:
+        print(f"Stock '{stock_name}' not found.")
 
-
-def update_surplus_worksheet(data):
-    """
-    Update surplus worksheet, add new row with the list data provided.
-    """
-    print('Updating surplus worksheet...\n')
-    surplus_worksheet = SHEET.worksheet('surplus')
-    surplus_worksheet.append_row(data)
-    print("surplus worksheet updated successfully.\n")
-
-
-def update_sales_worksheet(data):
-    """
-    Update sales worksheet, add new row with the list data provided.
-    """
-    print('Updating sales worksheet...\n')
-    sales_worksheet = SHEET.worksheet('sales')
-    sales_worksheet.append_row(data)
-    print("sales worksheet updated successfully.\n")
+#def show_low_performers():
 
 
-def update_worksheet(data, worksheet):
-    """
-    Update worksheets, add new row with the list data provided.
-    """
-    print(f'Updating {worksheet} worksheet...\n')
-    worksheet_to_update = SHEET.worksheet(worksheet)
-    worksheet_to_update.append_row(data)
-    print(f'{worksheet} worksheet updated successfully.')
+#def show_top_performers():
 
 
-def calculate_surplus_data(sales_row):
-    """
-    Compare sales with stock and calculate the surplus for each item type.
-
-    The surplus is defined as the sales figure subtracted from the stock:
-    - Positive surplus indicates waste
-    - Negative surplus indicates extra made when stock was sold out.
-    """
-    print('Calculating surplus data... \n')
-    stock = SHEET.worksheet('stock').get_all_values()
-    stock_row = stock[-1]
-
-    surplus_data = []
-    for stock, sales in zip(stock_row, sales_row):
-        surplus = int(stock) - sales
-        surplus_data.append(surplus)
-    
-    return surplus_data
+#def calculate_profit_loss():
 
 
-def get_last_5_entries_sales():
-    """
-    """
-    sales = SHEET.worksheet('sales')
-
-
-    columns = []
-    for ind in range(1,7):
-        column = sales.col_values(ind)
-        columns.append(column[-5:])
-    return columns
+print('Welcome to Stock Analyst. Get an overview of your portoflio')
 
 def main():
-    """
-    Run all program functions
-    """
-    data = get_sales_data()
-    sales_data = [int(num) for num in data]
-    update_worksheet(sales_data, 'sales')
-    calculate_surplus_data(sales_data)
-    new_surplus_data = calculate_surplus_data(sales_data)
-    update_worksheet(new_surplus_data,'surplus')
+    show_portfolio()
 
+    while True:
+        print("\nOptions:")
+        print("1: Delete a stock column")
+        print("2: Add a new stock column")
+        print("3: Adjust the multiplicator")
+        print("4: Exit")
 
-print('Welcome to Love Sandwiches Data Automation')
-#main()
-
-sales_columns = get_last_5_entries_sales()
+        choice = input("Choose an option (1, 2, 3, 4): ")
+        
+        if choice == '1':
+            delete_stock_column()
+        elif choice == '2':
+            add_stock_column()
+        elif choice == '3':
+            adjust_multiplicator()
+        elif choice == '4':
+            break
+        else:
+            print("Invalid option. Please choose again.")
+ 
+main()
