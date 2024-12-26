8b716efb3676cbb35ac9709b30060ba90168765e
Florian Guldner <florian.guldner@googlemail.com>
2024-12-26 14:12:58 +0000

    restructure code and delete performer functions

--- run_backup_25.12..py
+++ run_backup_25.12..py
@@ -27,6 +27,7 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('stock_analyst')
 
 stock_portfolio = SHEET.worksheet('stock_portfolio')
+stock_daily_update = SHEET.worksheet('stock_daily_update')
 profit_loss_sheet = SHEET.worksheet('profit_loss')
 
 
@@ -41,14 +42,10 @@ def show_portfolio():
     header = stock_portfolio.row_values(1)
     # Get 2. row of the sheet
     shares_row = stock_portfolio.row_values(2)
-    # Get 4. row of the sheet
+    # Get 2. row of the sheet
     purchase_price = stock_portfolio.row_values(4)
-    # Get 3. row of the sheet
-    profit_loss_percentage = profit_loss_sheet.row_values(3)
     # Get 2. row of the sheet
     profit_loss = profit_loss_sheet.row_values(2)
-    # Get 5. row of sheet
-    current_price = stock_portfolio.row_values(5)
 
     x = PrettyTable()
 
@@ -56,8 +53,7 @@ def show_portfolio():
         "stock name",
         "shares",
         "purch. price",
-        "current price",
-        "surplus p.",
+        "curr. price",
         "surplus total"
     ]
 
@@ -72,13 +68,20 @@ def show_portfolio():
 
         purchase = purchase_price[col_index - 1]  # Get purchase price
 
-        current = current_price[col_index - 1]  # Get current price
+        # Get the column values from stock_daily_update
+        column_values = stock_daily_update.col_values(col_index)[1:]
+
+        # Find the last non-empty value in the column
+        for value in reversed(column_values):
+            if value:
+                current = value
+                break
+        else:
+            current = purchase  # no values, then purchase price is used
 
         calculate_profit_loss()
         profloss = profit_loss[col_index - 1]  # Get total profit or loss
-        profloss_percentage = profit_loss_percentage[col_index - 1]  # Get profit or loss percentage
-
-        x.add_row([stock_name, shares, purchase, current, profloss_percentage, profloss])
+        x.add_row([stock_name, shares, purchase, current, profloss])
 
     print(x)
 
@@ -103,6 +106,7 @@ def add_stock_column():
     # add the new stock to all three sheets
     # Use api_call_with_retry for all update operations
     api_call_with_retry(stock_portfolio.update_cell, 1, last_column + 1, new_stock_name)
+    api_call_with_retry(stock_daily_update.update_cell, 1, last_column + 1, new_stock_name)
     api_call_with_retry(profit_loss_sheet.update_cell, 1, last_column + 1, new_stock_name)
     print(f"New column for stock '{new_stock_name}' added.")
 
@@ -130,23 +134,25 @@ def add_stock_column():
     try:
         purchase_price = float(purchase_price)
         api_call_with_retry(stock_portfolio.update_cell, 4, last_column + 1, purchase_price)
+        api_call_with_retry(stock_daily_update.update_cell, 2, last_column + 1, purchase_price)
         print(f"Purchase price of '{new_stock_name}' added.")
     except ValueError:
         print("Invalid input. Please enter a float number.")
 
-    # add current price
-    api_call_with_retry(stock_portfolio.update_cell, 5, last_column + 1, purchase_price)
-
     find_stock_symbol()
+    # fill blank rows in sheet stock_daily_update
+    # API_stock_daily_update()
 
 
 def delete_stock_column():
     # delete stock name, header and column on all sheets
     stock_name = input("Enter the name of the stock you want to delete:\n ")
     cell = stock_portfolio.find(stock_name)
+    cell1 = stock_daily_update.find(stock_name)
     cell2 = profit_loss_sheet.find(stock_name)
     if cell:
         stock_portfolio.delete_columns(cell.col)
+        stock_daily_update.delete_columns(cell1.col)
         profit_loss_sheet.delete_columns(cell2.col)
         print(f"Column for stock '{stock_name}' deleted.")
     else:
@@ -179,13 +185,79 @@ def adjust_multiplicator():
         print(f"Stock '{stock_name}' not found.")
 
 
+def show_top_performers():
+    header = stock_daily_update.row_values(1)
+    stocks_increasing = []
+
+    for col_index in range(1, len(header) + 1):
+        column_values = stock_daily_update.col_values(col_index)[1:]
+
+        # Check if there are at least 3 values and they are all numeric
+        if (
+            len(column_values) >= 3 and
+            column_values[-1] and
+            column_values[-2] and
+            column_values[-3]
+        ):
+            last_value = column_values[-1]
+            second_last_value = column_values[-2]
+            third_last_value = column_values[-3]
+
+            # Check if the last three values are strictly increasing
+            if (
+                third_last_value < second_last_value < last_value
+            ):
+                stocks_increasing.append(header[col_index - 1])
+
+        else:
+            print(
+                f"Not enough data provided for {header[col_index - 1]} "
+                "Please add those data in the sheet manually."
+            )
+
+    return stocks_increasing
+
+
+def show_low_performers():
+    header = stock_daily_update.row_values(1)
+    stocks_decreasing = []
+
+    for col_index in range(1, len(header) + 1):
+        column_values = stock_daily_update.col_values(col_index)[1:]
+
+        # Check if there are at least 3 values
+        if (
+            len(column_values) >= 3 and
+            column_values[-1] and
+            column_values[-2] and
+            column_values[-3]
+        ):
+            last_value = column_values[-1]
+            second_last_value = column_values[-2]
+            third_last_value = column_values[-3]
+
+            # Check if the last three values are strictly decreasing
+            if third_last_value > second_last_value > last_value:
+                stocks_decreasing.append(header[col_index - 1])
+
+        else:
+            print(
+                f"Not enough data provided for {header[col_index - 1]} "
+                "Please add those data in the sheet manually."
+            )
+
+    return stocks_decreasing
+
+
 def calculate_profit_loss():
-    header = stock_portfolio.row_values(1)  # Get the column headers
+    header = stock_daily_update.row_values(1)
     rows = stock_portfolio.get_all_values()  # Fetch all rows in one call
     purchase_price_column = rows[3]  # Row for purchase prices
-    current_price_column = rows[4] # Row for current prices
     multiplicator_row = rows[1]  # Row for shares
     
+    # Fetch all column values in one call
+    all_column_values = stock_daily_update.get_all_values()[1:]  # Skip header row
+
     # List for profit and loss values
     surplus_data = []
     percentage_data = []
@@ -198,17 +270,19 @@ def calculate_profit_loss():
             print(f"Error: Purchase price missing for column {header[col_index - 1]}")
             continue
 
-        # Access the current price safely
-        try:
-            current_price = current_price_column[col_index - 1]
-        except IndexError:
-            print(f"Error: Purchase price missing for column {header[col_index - 1]}")
+        # Extract the specific column values wihtout API request
+        column_values = [row[col_index - 1] for row in all_column_values]  
+
+        # Check for empty column or missing data
+        if not column_values or len(column_values) < 1:
+            print(f"Error: Missing/wrong data for column {header[col_index - 1]}")
             continue
 
+        last_column_value = column_values[-1]
 
         try:
             first_value = float(purchase_price)
-            last_value = float(current_price)
+            last_value = float(last_column_value)
         except ValueError:
             print(f"Non-numeric data found in column: {header[col_index - 1]}")
             continue
@@ -260,6 +334,21 @@ def column_check():
         )
         return False
 
+    # check stock_daily_update
+    header = stock_daily_update.row_values(1)
+    purchase_price = stock_daily_update.row_values(2)
+
+    if len(header) == len(purchase_price):
+        return True
+
+    else:
+        print(
+            f"Check sheet stock_daily_update. The lenght of the rows "
+            "are not equal. Data is missing. "
+            "Adjust it manually in the sheet stock_daily_update."
+        )
+        return False
+
     # check profit_loss_sheet
     header = profit_loss_sheet.row_values(1)
     surplus = profit_loss_sheet.row_values(2)
@@ -277,7 +366,7 @@ def column_check():
 
 
 def provide_updated_data():
-    # show data stock in the terminal
+    # show relevant data for stock_daily_update in the terminal
     symbol = stock_portfolio.row_values(3)  # Get the abbrevation of the stocks
 
     for col_index in range(len(symbol)):
@@ -306,53 +395,7 @@ def provide_updated_data():
                     f"The latest price from {stock_name_symbol} is: "
                     f"{float(desired_value):.2f}. "
                     "You can add it to the last column of "
-                    "googlesheet 'stock_portfolio'."
-                )
-
-            else:
-                print(f"Not enough data points for {stock_name_symbol}.")
-
-        else:
-            print(
-                f"for {stock_name_symbol}. "
-                "Standard API rate limit is 25 requests per day. "
-                "Check the prices here: https://finance.yahoo.com/quote/MSFT/history/"
-            )
-
-
-def add_updated_data():
-    symbol = stock_portfolio.row_values(3)  # Get the abbrevation of the stocks
-
-    for col_index in range(len(symbol)):
-
-        stock_name_symbol = symbol[col_index]  # Get one abbr. for each column
-        url = (
-            f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&'
-            f'symbol={stock_name_symbol}&apikey='
-            f'{os.environ.get("alphaventage-api")}'
-        )
-        r = requests.get(url)
-        data = r.json()
-
-        # Check if 'Time Series (Daily)' key exists in the JSON response
-        if 'Time Series (Daily)' in data:
-            time_series = data['Time Series (Daily)']
-            dates = list(time_series.keys())
-            dates.sort(reverse=True)
-
-            if len(dates) > 1:
-                second_date = dates[1]
-                second_date_data = time_series[second_date]
-                desired_value = second_date_data['4. close']
-
-
-                # Update the sheet with the new stock price
-                # Adjust col_index to 1-based index for update_cell
-                api_call_with_retry(stock_portfolio.update_cell, 5, col_index + 1, desired_value)
-                print(
-                    f"The latest price from {stock_name_symbol} is updated."
-                    f"{float(desired_value):.2f}. "
-                    "is added to googlesheet."
+                    "googlesheet 'stock_daily_update'."
                 )
 
             else:
@@ -479,32 +522,52 @@ def main():
         print("1: Add a new stock")
         print("2: Delete a stock")
         print("3: Adjust the number of shares")
-        print("4: Show latest stock prices")
-        print("5: Add latest stock prices")
-        print("6: Exit")
+        print("4: Show current top performer")
+        print("5: Show current low performer")
+        print("6: Show latest stock prices")
+        print("7: Exit")
 
-        choice = input("Choose an option (1, 2, 3, 4, 5, 6): ")
+        choice = input("Choose an option (1, 2, 3, 4, 5, 6, 7): ")
 
         if choice == '1':
             add_stock_column()
-            time.sleep(2)
+            time.sleep(5)
             clear()
         elif choice == '2':
             delete_stock_column()
-            time.sleep(2)
+            time.sleep(5)
             clear()
         elif choice == '3':
             adjust_multiplicator()
-            time.sleep(2)
-            clear()
-        elif choice == '4':
-            provide_updated_data()
-            time.sleep(15)
-        elif choice == '5':
-            add_updated_data()
             time.sleep(5)
             clear()
+        elif choice == '4':
+            increasing_stocks = show_top_performers()
+            if increasing_stocks:
+                print(
+                    "Current top performer(s) of the last three days: ",
+                    ", ".join(increasing_stocks)
+                )
+            else:
+                print("No stocks with three times increase availabe.")
+            time.sleep(10)
+            clear()
+        elif choice == '5':
+            decreasing_stocks = show_low_performers()
+            if decreasing_stocks:
+                print(
+                    "Current low performer(s) of last three days: ",
+                    ", ".join(decreasing_stocks)
+                )
+            else:
+                print("No stocks with three times decrease availabe.")
+            time.sleep(10)
+            clear()
         elif choice == '6':
+            provide_updated_data()
+            time.sleep(20)
+            clear()
+        elif choice == '7':
             break
         else:
             print("Invalid option. Please choose again.")
